4780ffa6997fd9e52dc34a6dc5d99d9fbc6ccf53
Logic-seeker <shobhabstha@gmail.com>
2012-02-15 15:06:45 +0545

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NationalComputer::Application.config.secret_token = 'e7c65767a6fe223599421abdf133804664e1edf2051b14c37daf1629e9ab7a2187cfd0b04714a119a2e3dcac170ab4f760d34bca4cef799a155c07bbcaa76aa3'
